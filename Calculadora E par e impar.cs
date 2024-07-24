715b82c6cb2cb470201a0ae08ae79624bb37f948
nicholasdouglas <158283953+nicholasdouglas@users.noreply.github.com>
2024-07-24 10:15:08 -0300

    Add files via upload

---
+++ Calculadora E par e impar.cs
@@ -0,0 +1,56 @@
+﻿/*Código por: 
+  
+ Nicholas Basso
+
+ Romeu Tamarozi 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOEVER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite Dois Numeros");
+            int num1;
+            int num2;
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qual op");
+            char op = Convert.ToChar(Console.ReadLine());
+            int valF = 0;
+            switch (op)
+            {
+                case '+':
+                    valF = num1 + num2;
+                    break;
+                case '-':
+                    valF = num1 - num2;
+                    break;
+                case '*':
+                    valF = num1 * num2;
+                    break;
+                case '/':
+                    valF = num1 / num2;
+                    break;
+            }
+
+
+            Console.WriteLine($"O resultado da operação {valF}");
+
+            if (valF % 2 == 0)
+                Console.WriteLine("O numero é par");
+            else
+                Console.WriteLine("O numero é impar");
+
+            Console.ReadKey();
+        }
+    }
+}
